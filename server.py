5ecd6e4883e11f16a8bc728f17e1995be0576d59
PLacek7777 <cieszynski.rd@gmail.com>
2023-01-28 13:15:38 +0100

    Update server.py

--- server.py
+++ server.py
@@ -1,50 +1,39 @@
+from configparser import ConfigParser
 import threading
+import logging
 import socket
-import time
 
-PORT = 5555
-SERVER = socket.gethostbyname(socket.gethostname())
+conf = ConfigParser()
+conf.read("conf.config")
+confData = conf["SERVER"]
+
+PORT = int(confData["PORT"])
+SERVER = confData["SERVER"]
 ADDR = (SERVER, PORT)
 
-HEADER = 64
-FORMAT = "utf-8"
-DISCONECT_COMADN = "?disconect"
+HEADER = int(confData["HEADER"])
+FORMAT = confData["FORMAT"]
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(ADDR)
 
+logging.basicConfig(filename="Server.log", level=logging.DEBUG, format='%(asctime)s >> %(message)s')
+
 def handleClient(conn, addr):
-    t = time.strftime("%H:%M:%S", time.localtime())
-    print(f'[SERVER : {t}]>> New conection: {addr}')
-    conected = True
-    while conected:
-        msgLeangt = conn.recv(HEADER).decode(FORMAT)
-        if msgLeangt:
-            msgLeangt = int(msgLeangt)
-            msg = conn.recv(msgLeangt).decode(FORMAT)
-            if str.lower(msg) == "?disconect":
-                conected = False
-            t = time.strftime("%H:%M:%S", time.localtime())
-            print(f'[{addr} : {t}]>> {msg}')
-            conn.send("Recvd".encode(FORMAT))
-    
-    conn.close()
-    t = time.strftime("%H:%M:%S", time.localtime())
-    print(f"[SERVER : {t}]>> {addr}: disconected")
-    print(f'[SERVER : {t}]>> Active conections: {threading.active_count()-2}')
+    while True:
+        try:
+            name = conn.recv(HEADER).decode(FORMAT)
+            logging.info(f'Connected: {addr[0]}: {name}')
+        except:
+            conn.close()
+            logging.info(f'Disconnected: {addr[0]}: {name}')
+            break
 
 def start():
     server.listen()
-    t = time.strftime("%H:%M:%S", time.localtime())
-    print(f"[SERVER : {t}]>> Server started!")
-    print(f'[SERVER : {t}]>> IPv4: {SERVER}')
+    logging.info("Started")
     while True:
         conn, addr = server.accept()
         thread = threading.Thread(target=handleClient, args=(conn, addr))
         thread.start()
-        t = time.strftime("%H:%M:%S", time.localtime())
-        print(f'[SERVER : {t}]>> Active conections: {threading.active_count()-1}')
-
-t = time.strftime("%H:%M:%S", time.localtime())
-print(f"[SERVER : {t}]>> Server is starting...")
 start()
